74c402886b0a8e228ce24dc3d6317463b9e28f23
Michael Watts <wattsjmichael@gmail.com>
2020-11-17 13:34:32 -0800

    Adds more stuff

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -3,37 +3,108 @@ import NewTicketForm from "./NewTicketForm";
 import TicketList from "./TicketList";
 import Steps from "./Steps";
 import Help from "./Help";
+import Minutes from "./Minutes";
 
 class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      ticketListVisibleOnPage: true,
+      stepsVisibleOnPage: false,
+      helpVisibleOnPage: false,
+      minutesVisibleOnPage: false,
       formVisibleOnPage: false
     };
-    this.handleClick = this.handleClick.bind(this); //new code here
+    this.handleForwardClick = this.handleForwardClick.bind(this);
+    this.handleBackClick = this.handleBackClick.bind(this); //new code here
+    
   }
 
-  handleClick() {
+  handleBackClick() {
+    console.log("before click ticket list "+this.state.ticketListVisibleOnPage);
+    console.log("before click steps "+this.state.stepsVisibleOnPage);
+    console.log("before click help "+this.state.helpVisibleOnPage);
+    console.log("before click minutes "+this.state.minutesVisibleOnPage);
+    console.log("before click form "+this.state.formVisibleOnPage);
     this.setState(prevState => ({
-      formVisibleOnPage: !prevState.formVisibleOnPage
+      formVisibleOnPage: !prevState.formVisibleOnPage,
+      stepsVisibleOnPage: !prevState.stepsVisibleOnPage
+      
     }));
   }
 
+  handleForwardClick = () => {
+    console.log("before click ticket list "+this.state.ticketListVisibleOnPage);
+    console.log("before click steps "+this.state.stepsVisibleOnPage);
+    console.log("before click help "+this.state.helpVisibleOnPage);
+    console.log("before click minutes "+this.state.minutesVisibleOnPage);
+    console.log("before click form "+this.state.formVisibleOnPage);
+    if(this.state.ticketListVisibleOnPage){
+      this.setState({ticketListVisibleOnPage: false, stepsVisibleOnPage: true})
+      return;
+    }
+    else if(this.state.stepsVisibleOnPage){
+      // console.log("two");
+      this.setState({stepsVisibleOnPage: false, helpVisibleOnPage: true})
+      return;
+    }
+    else if(this.state.helpVisibleOnPage){
+      // console.log("three");
+      this.setState({helpVisibleOnPage: false, minutesVisibleOnPage: true})
+      return;
+    }
+    else if(this.state.minutesVisibleOnPage){
+      // console.log("four");
+      this.setState({minutesVisibleOnPage: false, formVisibleOnPage: true})
+      return;
+    }
+    else {
+      // console.log("five");
+      // console.log(this.state.ticketListVisibleOnPage);
+      this.setState({formVisibleOnPage: false, ticketListVisibleOnPage: true})
+      return;
+    }
+    };
+  
+
   render() {
     let currentVisibleState = null;
-    let buttonText = null;
-    if (this.state.formVisibleOnPage) {
+    let buttonBackText = null;
+    let buttonForwardText = null;
+    let addTicketButton = null;
+    
+    if (this.state.stepsVisibleOnPage){
+      currentVisibleState = <Steps />;
+      buttonBackText = "Return to Ticket List"; // new code
+      buttonForwardText = "Go to Help"; // new code
+    }
+    else if (this.state.helpVisibleOnPage){
+      currentVisibleState = <Help />;
+      buttonBackText = "Return to Steps"; // new code
+      buttonForwardText = "Go to Minutes"; // new code
+    }
+    else if (this.state.minutesVisibleOnPage){
+      currentVisibleState = <Minutes />;
+      buttonBackText = "Return to Help"; // new code
+      buttonForwardText = "Go to Form"; // new code
+    }
+    else if (this.state.formVisibleOnPage) {
       currentVisibleState = <NewTicketForm />;
-      buttonText = "Return to Ticket List"; // new code
-    } else {
+      buttonBackText = "Return to Minutes"; // new code
+      buttonForwardText = "Return to Ticket List"; // new code
+    } 
+    else {
       currentVisibleState = <TicketList />;
-      buttonText = "Add Ticket"; // new code
+      buttonBackText = "Add Ticket"; // new code
+      buttonForwardText = "Go to Steps"; // new code
+      addTicketButton = <button onClick={this.handleForwardClick}>Add ticket</button> // new code
     }
     return (
       <React.Fragment>
         {currentVisibleState}
-        <button onClick={this.handleClick}>{buttonText}</button>
+        <button onClick={this.handleBackClick}>{buttonBackText}</button>
+        <button onClick={this.handleForwardClick}>{buttonForwardText}</button>
       </React.Fragment>
     );
   }
